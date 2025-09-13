99304136a33e6a30c97c84f095c6fe64d962916a
gastbob40 <gastbob40@gmail.com>
2025-09-13 19:15:06 +0200

    chore(ci): deploy nats ui to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react(), tailwindcss(),],
   resolve: {
     alias: {
